6515b39c429fd46de2e62f833b5651c8da4f782b
Navas Moosa <kpnavasmoosa@gmail.com>
2025-02-03 00:30:51 +0530

    modified signup

--- models/userModel.js
+++ models/userModel.js
@@ -2,13 +2,13 @@ const pool = require('../config/db'); // Assuming you're using PostgreSQL with a
 
 // Save User to the Database
 const signupUser = async (userData) => {
-  const { full_name, email, phone_number, district, state, dob, blood_group, donate_blood, password } = userData;
+  const { full_name, email, phone_number, district, dob, blood_group, donate_blood, password } = userData;
 
   const query = `
-    INSERT INTO signup (full_name, email, phone_number, district, state, dob, blood_group, donate_blood, password)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id
+    INSERT INTO signup (full_name, email, phone_number, district, dob, blood_group, donate_blood, password)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id
   `;
-  const values = [full_name, email, phone_number, district, state, dob, blood_group, donate_blood, password];
+  const values = [full_name, email, phone_number, district, dob, blood_group, donate_blood, password];
 
   const result = await pool.query(query, values);
   return result.rows[0];
--- routes/vehicleRoutes.js
+++ routes/vehicleRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { registerVehicle } = require('../controllers/vehicleController');
+const { registerVehicle, getVehicles } = require('../controllers/vehicleController');
 const router = express.Router();
 
-
+// POST route for registering a new vehicle
 router.post('/', registerVehicle);
+
+// GET route for fetching the list of all vehicles
+router.get('/', getVehicles);
 
 module.exports = router;
